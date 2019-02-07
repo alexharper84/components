cb17d2742ddca6784c67ffbdb7b06c527d5f5a5f
alexharper84 <alexharper84@gmail.com>
2019-02-07 02:28:03 -0800

    refactoring...

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comments">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <span className="date">Today at 6pm</span>
-        </div>
-        <div className="text">nice blog post</div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };

6ce29b6ffeea51a46986564153fe25afe8365239
alexharper84 <alexharper84@gmail.com>
2019-02-06 18:02:43 -0800

    code sweetening

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comments">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6pm</span>
+        </div>
+        <div className="text">nice blog post</div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));

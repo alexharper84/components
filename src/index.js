0ef4dcd609f6f2bca6bb57156376e3cf8ed825e5
alexharper84 <alexharper84@gmail.com>
2019-02-08 08:34:59 -0800

    reuseable component refactor

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="today at 4pm"
-        userComment="Cool bro"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="today at 5pm"
-        userComment="nice bro"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="today at 6pm"
-        userComment="sweet bro"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="today at 4pm"
+          userComment="Cool bro"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="today at 5pm"
+          userComment="nice bro"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="today at 6pm"
+          userComment="sweet bro"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

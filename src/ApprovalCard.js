d670735cabbd9aa5aea5ff8c2404d95c3cf465f9
alexharper84 <alexharper84@gmail.com>
2019-02-08 00:53:05 -0800

    card code

---
+++ src/ApprovalCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ApprovalCard = () => {
+  return (
+    <div className="ui card">
+      <div className="content">Test</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Yes</div>
+          <div className="ui basic red button">No</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApprovalCard;
